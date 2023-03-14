b1108466181e7acaa2cd362be5f396850a10ab14
Ne8o <127225903+Ne8o@users.noreply.github.com>
2023-03-14 13:27:23 -0700

    Add files via upload

---
+++ raodenoba.py
@@ -0,0 +1,9 @@
+#write text
+text = str(input("write something :"))
+#calculate len
+l_text = len(text)
+# run function
+if l_text !=0:
+    print("in your text is","-",text,"is", l_text,"symbol")
+else:
+    print("what are you w8ing for ?!")
